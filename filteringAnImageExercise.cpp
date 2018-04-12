3e44a5a231873c10c1792b00cbea889bb289de72
marcrabat <marc.rabat01@estudiant.upf.edu>
2018-04-12 11:19:35 +0200

    Repo Inicialitzat

---
+++ filteringAnImageExercise.cpp
@@ -0,0 +1,36 @@
+void filteringAnImageExercise()
+{
+    // Create two instances of the film class with the same resolution
+    int resX, resY;
+    resX = 512;
+    resY = 512;
+    Film f1(resX, resY);
+    Film f2(resX, resY);
+
+    // Create the original image
+    //  Draw a circle centered at centerX, centerY (in pixels, image space)
+    //   and with ray r (also in pixels)
+    int centerX = resX / 2;
+    int centerY = resY / 2;
+    int r = std::min(centerX, centerY)/2;
+    for(int lin=0; lin<resX; lin++)
+    {
+        for(int col=0; col<resY; col++)
+        {
+            // Use the equation of the sphere to determine the pixel color
+            if( (lin-centerX)*(lin-centerX) + (col-centerY)*(col-centerY) < r*r )
+                f1.setPixelValue(col, lin, Vector3D(1, 1, 0));
+        }
+    }
+
+    // Filter-related variables
+    // Declare here your filter-related variables
+    // (e.g., FILTER SIZE)
+	//(...)
+
+	// Implement here your image filtering algorithm
+	//(...)
+
+    // DO NOT FORGET TO SAVE YOUR IMAGE!
+	//(...)
+}
